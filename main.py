82631de1734998168605c7b3399650fbc705e667
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2024-01-04 17:29:12 -0500

    code cleanup

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def process(path):
   
   # Use BeautifulSoup to parse the HTML content
   soup = bs.BeautifulSoup(html_content, 'html.parser')
-  #results = soup.find(id="z_l")
   
   names = soup.find_all("th", {"class": "d_ich d2l-table-cell-first", "scope": "row"})
 
