9439d995c0984d96414e9e9727e2fd545553fc1d
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2024-01-04 15:56:40 -0500

    refactored code

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import bs4 as bs
 
-html_file_path = './classes/1.html'
+def process(path):
+  html_file_path = path 
+  
+  # Read the HTML file content
+  with open(html_file_path, 'r', encoding='utf-8') as file:
+      html_content = file.read()
+  
+  # Use BeautifulSoup to parse the HTML content
+  soup = bs.BeautifulSoup(html_content, 'html.parser')
+  #results = soup.find(id="z_l")
+  
+  names = soup.find_all("th", {"class": "d_ich d2l-table-cell-first", "scope": "row"})
 
-# Read the HTML file content
-with open(html_file_path, 'r', encoding='utf-8') as file:
-    html_content = file.read()
+  extract = []
+  
+  for clean in names:
+    print(clean.text)
 
-# Use BeautifulSoup to parse the HTML content
-soup = bs.BeautifulSoup(html_content, 'html.parser')
-#results = soup.find(id="z_l")
-
-names = soup.find_all("th", {"class": "d_ich d2l-table-cell-first", "scope": "row"})
-
-for clean in names:
-  print(clean.text)
+process('./classes/2.html')
